6b42181487c99ab1048d8efc5f43e2fac6dd39c9
Khanh Chung <khanh.d.chung@gmail.com>
2022-12-18 15:03:19 -0800

    2.13*: Data for countries, step2

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,6 +1,19 @@
+import { useState, useEffect } from 'react'
 import Country from './Country'
 
 const Countries = ({ countries }) => {
+  const [shownCountries, setShownCountries] = useState([])
+
+  useEffect(() => {
+    setShownCountries(countries.map(c => false))
+  }, [countries])
+
+  const handleShowButtonClick = index => {
+    const updatedShownCountries = [...shownCountries]
+    updatedShownCountries[index] = !updatedShownCountries[index]
+    setShownCountries(updatedShownCountries)
+  }
+
   // Show detail of a single country
   if (countries.length === 1) {
     return <Country country={countries[0]} />
@@ -13,8 +26,19 @@ const Countries = ({ countries }) => {
 
   return (
     <div>
-      {countries.map(country => 
-        <p key={country.name.common}>{country.name.common}</p>
+      {countries.map((country, index) =>
+        <div key={country.name.common}>
+          {country.name.common}
+
+          <button onClick={() => handleShowButtonClick(index)}>
+            {shownCountries[index] ? 'cancel' : 'show'}
+          </button>
+
+          {shownCountries[index]
+            ? <Country country={country} />
+            : ''
+          }
+        </div>
       )}
     </div>
   )
